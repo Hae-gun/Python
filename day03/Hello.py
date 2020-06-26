78ad7261b0c10f3132918f1fa42c13b4813546ec
Hae-gun <newchk610@gmail.com>
2020-06-26 14:37:19 +0900

    day03

---
+++ day03/Hello.py
@@ -0,0 +1,49 @@
+# a = int(input('첫 번재 숫자를 입력하세요: '))
+# b = int(input('두 번재 숫자를 입력하세요: '))
+
+
+# print(type(a))
+# print(a+b)
+
+# a,b = input('문자열 두개를 입력하세요 (공백으로 구분)').split()
+
+# print('첫번째: '+a+'\n두번째: '+b)
+
+# a, b = input('숫자 두 개를 입력하세요: ').split()    
+# # 입력받은 값을 공백을 기준으로 분리
+# a = int(a)    # 변수를 정수로 변환한 뒤 다시 저장
+# b = int(b)    # 변수를 정수로 변환한 뒤 다시 저장
+ 
+# print(a + b)
+
+# a,b = map(int, input('숫자두개 입력: ').split(','))
+# print(a+b)
+
+
+# # print(a)
+
+# a=10
+# b=100
+# c=None
+# print(a)
+# print(b)
+# print(c)
+# a,b,c,d = map(int, input().split())
+# avg = (a+b+c+d)//4
+# print(avg)
+
+# print(1,2,3)
+# print('hello','world')
+
+# print(1,2,3,sep = ', ')
+# print(4,5,6,sep = ',')
+# print('hello','world',sep = '')
+# print(1920,1080,sep = 'x')
+
+# print(1,2,3,sep='\n')
+
+# print('1\n2\n3')
+
+print(1,end=' ')
+print(2,end=' ')
+print(3)
