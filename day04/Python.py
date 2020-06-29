ab9bcbd40f35b5031751cc9ba52370d6562a5093
Hae-gun <newchk610@gmail.com>
2020-06-30 00:24:04 +0900

    200630

---
+++ day04/Python.py
@@ -0,0 +1,25 @@
+# print(False and True)
+# print(False and False)
+# print(True or True)
+# print(True or False)
+
+
+single_quote = '''"안녕하세요."
+'파이썬'입니다.'''
+
+double_quote1 = """"Hello"
+'Python'"""
+
+double_quote2 = """Hello, 'Python'"""
+
+print(single_quote)
+print(double_quote1)
+print(double_quote2)
+
+
+print('Hello, world!')
+
+print("""'Python' is a \"programming language\"
+that lets you work quickly
+and
+integrate systems more effectively.""")
